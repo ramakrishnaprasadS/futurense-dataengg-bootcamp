56756fae5b93b7682fce8e27c0e1fc62ebfe281c
ramakrishnaprasadS <121850384+ramakrishnaprasadS@users.noreply.github.com>
2023-02-10 23:24:44 +0530

    assignment1 and 2 updated

--- labs/shell-scripts/classfiles/operators.sh
+++ labs/shell-scripts/classfiles/operators.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 a=$1
 b=$2
+chk=[ $a -eq $b ] 
 echo "sum of $a and $b is `expr $a + $b`"
 echo "product of $a and $b is `expr $a \* $b`"
-echo "arg1 == arg2 is `[$a == $b]`"
+echo "arg1 == arg2 is $chk"
