7de1fd890079cc8c87073fa2661b21f8280b77df
ramakrishnaprasadS <121850384+ramakrishnaprasadS@users.noreply.github.com>
2023-02-28 23:32:50 +0530

    ratings weather corespark asgn updated

---
+++ assignments/pyspark/pyspark-core/ratings.py
@@ -0,0 +1,8 @@
+from pyspark import SparkContext
+
+sc = SparkContext("local", "ratings app")
+
+l1 = sc.textFile("/mnt/c/Users/miles/Downloads/movies/ratings.csv")
+for rating,vals in l1.map(lambda x:x.split(',')).groupBy(lambda x:x[2]).collect():
+    if rating!="rating":
+        print(rating,len(vals))
--- assignments/pyspark/pyspark-core/weather.py
+++ assignments/pyspark/pyspark-core/weather.py
@@ -1,16 +1,23 @@
 from pyspark import SparkContext
 
-sc = SparkContext("local", "ratings app")
+sc = SparkContext("local", "weather app")
 
 lines = sc.textFile("/home/ram/futurense_hadoop-pyspark/labs/dataset/weather/weather_data.txt")
 
 lines1=lines.map(lambda x: x.split())
 lines2=lines1.map(lambda x:("max_temp",float(x[5])))
-lines3=lines2.reduceByKey(lambda x,y:max(x,y))
+linesmax=lines2.reduceByKey(lambda x,y:max(x,y))
 
 l1=lines.map(lambda x: x.split())
-l2=l1.map(lambda x:("min_temp",float(x[5])))
-l3=l2.reduceByKey(lambda x,y:min(x,y))
+l2=l1.map(lambda x:("min_temp",float(x[6])))
+lmin=l2.reduceByKey(lambda x,y:min(x,y))
 
-print(l3.collect())
-print(lines3.collect())
+for each in lines.flatMap(lambda x: [[x.split()[1]]+[x.split()[5]]]).map(lambda x: (x[0][4:6],float(x[1]))).reduceByKey(lambda a,b:min(a,b)).collect():
+    print(each[0],"min_temp",each[1])
+for each in lines.flatMap(lambda x: [[x.split()[1]]+[x.split()[6]]]).map(lambda x: (x[0][4:6],float(x[1]))).reduceByKey(lambda a,b:max(a,b)).collect():
+    print(each[0],"max_temp",each[1])
+
+print(lmin.collect())
+print(linesmax.collect())
+
+
